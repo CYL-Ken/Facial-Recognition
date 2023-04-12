686507e72f15bc90b93c03af2a09195050d6e725
CYL-Ken <ken.huang@cyl-tek.com>
2023-04-12 12:07:43 +0800

    Fix bugs

--- recognizer.py
+++ recognizer.py
@@ -4,7 +4,15 @@ import time
 import json
 import argparse
 import requests
-import numpy as np
+from threading import Timer
+
+
+class Door():
+    def __init__(self) -> None:
+        self.status = True
+
+    def enable_door(self):
+        self.status = True
 
 if __name__ == "__main__":
     print("<Facial Recognition Recognizer>")
@@ -12,7 +20,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Facial Recognition Recognizer")
     parser.add_argument("-w", "--weight", default="face.yml", type=str, help="Model Weight Path")
     parser.add_argument("-v", "--video", default="http://admin:admin@192.168.50.2/video.cgi?identify_key=984852984&pipe=0", type=str, help="Video Streaming Path")
-    parser.add_argument("-t", "--time", default=5, type=int, help="Waiting time between open door")
+    parser.add_argument("-t", "--time", default=3, type=int, help="Waiting time between open door")
     args = parser.parse_args()
     
     print(" - Prepare Face Detection and Recognition Models...")
@@ -23,6 +31,9 @@ if __name__ == "__main__":
     with open('name.json') as json_file:
         name_dict = json.load(json_file)
     
+    door = Door()
+    door_timer = Timer(args.time, door.enable_door)
+    
     while True:
         cap = cv2.VideoCapture('http://admin:admin@192.168.50.2/video.cgi?identify_key=984852984&pipe=0')
         if not cap.isOpened():
@@ -31,20 +42,21 @@ if __name__ == "__main__":
         while True:
             ret, img = cap.read()
             if not ret:
-                print(" - Cannot Receive Frame")
+                print(" - Cannot Receive Frame!")
                 break
 
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             faces = face_detector.detectMultiScale(gray)
             
-            print("No face")
+            print("No face", door.status)
             for (x, y, w, h) in faces:
                 cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 2)
                 id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-                if confidence < 40:
+                if confidence < 40 and door.status == True:
                     text = name_dict[str(id)]
                     print("Hello ", text)
                     response = requests.get(r"http://admin:admin@192.168.50.2/DP/doorunlock.ncgi?id=2635107228")
-                    time.sleep(5)
+                    door_timer.start()
+                    door.status = False
                 else:
                     print("Who are you?")
